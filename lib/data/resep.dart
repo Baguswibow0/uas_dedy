fdf8f847af15680ec6a3a74ecf375d994e847249
Baguswibow0 <bagus13320@gmail.com>
2024-01-27 06:48:45 +0700

    first init github

---
+++ lib/data/resep.dart
@@ -0,0 +1,106 @@
+class resep {
+  String name, htm, tutorial, image;
+
+  resep(
+      {required this.name,
+      required this.htm,
+      required this.tutorial,
+      required this.image});
+}
+
+List<resep> dataResep = [
+  resep(
+      name: ' Maulana Malik Ibrahim (Sunan gresik) ',
+      htm: 'Gresik',
+      tutorial:
+          'Nama aslinya adalah Maulana Malik Ibrahim. Tahun lahirnya tidak diketahui, Sunan Gresik bermukim di Gresik untuk menyiarkan ajaran Islam hingga akhir hayatnya pada tanggal 12 Rabiul awwal 822 H, bertepatan dengan 8 April 1419 M dan dimakamkan di desa Gapura kota Gresik.'
+
+'Ia adalah seorang ahli tata negara yang menjadi penasehat raja, guru para pangeran, dan juga dermawan terhadap fakir miskin.'
+
+'Makamnya banyak diziarahi masyarakat hingga sekarang. Sunan Gresik dianggap sebagai penyiar Islam pertama di tanah Jawa, sehingga dianggap sebagai Ayah dari Walisongo.'
+
+'Berdasarkan silsilahnya, Sunan Gresik merupakan keturunan ke-22 dari Nabi Muhammad SAW. melalui Siti Fatimah yang menikah dengan Ali bin Abi Thalib.'
+
+'Sunan Gresik merupakan bapak dari Sunan Ampel. Selain itu, beliau juga merupakan kakek dari Sunan Bonang dan Sunan Drajat.',
+      image: 'assets/sunan-gresik.jpg'),
+  resep(
+      name: 'Raden Rahmat(Sunan Ampel).',
+      htm: 'Kerajaan Champa Vietnam',
+      tutorial:
+          'Nama aslinya adalah Raden Rahmat yang lahir di Kerajaan Champa, Vietnam. Ia adalah putra cucu Raja Champa, ayahnya bernama Ibrahim As-Samarkandi yang menikah dengan Puteri Raja Champa yang bernama Dewi Candra Wulan. Raden Rahmat ke tanah Jawa langsung ke Majapahit, karena bibinya Dewi Dwara Wati diperistri Raja Brawijaya.'
+
+'Beliau merupakan tokoh yang menyebarkan ajaran Islam di Jawa Timur. Sunan Ampel merupakan sunan pertama di Demak dan pemimpin asli Wali Songo.'
+
+'Raden Rahmat berhenti di Tuban dan di tempat itu beliau berkenalan dengan dua tokoh masyarakat yaitu Ki Wiryo Sarojo dan Ki Bang Kuning, yang keduanya kemudian masuk Islam beserta keluarganya. Dengan masuk Islamnya Ki Wiryo Sarojo dan Ki Bang Kuning, usaha Sunan Ampel semakin mudah dalam mendekati masyarakat dan melakukan dakwah Islam, sedikit demi sedikit mengajarkan Ketauhidan dan Ibadah.'
+
+'Sunan Ampel wafat pada tahun 1406 M. Beliau dimakamkan di Kompleks Masjid Ampel, Surabaya. Sampai sekarang makam beliau banyak dikunjungi peziarah dari berbagai daerah di seluruh pelosok Indonesia.',
+      image: 'assets/sunan-ampel.jpg'),
+  resep(
+      name: 'Maulana Makhdum Ibrahim(Sunan Bonang)',
+      htm: 'Tuban',
+      tutorial:
+          'Sunan Bonang adalah putra dari Sunan Ampel dari istri yang bernama Dewi Candrawati. Ia dikenal sebagai ahli Ilmu Kalam dan Ilmu Tauhid.'
+
+'Maulana Makhdum Ibrahim banyak belajar di Pasai, kemudian sekembalinya dari Pasai, Maulana Makhdum Ibrahim mendirikan pesantren di daerah Tuban.'
+
+'Santri yang belajar pada pesantren Maulana Makhdum Ibrahim, berasal dari seluruh penjuru daerah di tanah air. Sunan Bonang meninggal pada tahun 1525 dan dimakamkan di Tuban, daerah pesisir utara Jawa yang menjadi basis perjuangan dakwahnya.'
+
+'Semasa hidupnya, Sunan Bonang kerap berdakwah melalui kesenian agar bisa menarik masyarakat Jawa untuk memeluk agama Islam. Pernah mendengar lagu Wijil atau Tombo Ati yang dipopulerkan oleh Opick? Kedua lagu tersebut adalah hasil karya Sunan Bonang. Untuk menambah unsur Islami dalam lagu-lagu yang digubahnya, Sunan Bonang memasukkan rebab dan bonang sebagai pelengkap dari gemelan Jawa. Oleh sebab itulah ia mendapatkan julukan Sunan Bonang.'
+
+'Selain itu ia mempunyai keunikan dalam berdakwah dengan cara mengubah nama-nama dewa dengan nama-nama malaikat sebagaimana yang dikenal dalam Islam.Hal ini dimaksudkan sebagai upaya persuasif terhadap penganut ajaran Hindu dan Budha yang telah lama dipeluk sebelumnya.',
+      image: 'assets/sunan-bonang.jpg'),
+  resep(
+      name: ' Radem Qasim(Sunan Drajat)',
+      htm: 'Paciran Lamongan',
+      tutorial:
+          'Selain Makhdum Ibrahim atau Sunan Bonang, Raden Qasim yang juga putra dari Sunan Ampel dikenang oleh masyarakat di seluruh Tanah Air sebagai Sunan Drajat. Dalam misinya untuk menyebarkan agama Islam di Indonesia, ia menggunakan kegiatan sosial sebagai ujung tombaknya. Ia berdakwah di daerah Drajad kecamatan Paciran Lamongan.'
+
+'Ia mempelopori penyantunan anak-anak yatim dan orang-orang sakit. Selain itu Sunan Drajat banyak berdakwah kepada masyarakat umum. Ia sangat mengedepankan sikap dermawan, kerja keras dan meningkatkan kemakmuran rakyat sebagai pengamalan agama Islam',
+      image: 'assets/sunan-drajat.jpg'),
+  resep(
+      name: 'a’far Shadiq (Sunan Kudus)',
+      htm: 'blora',
+      tutorial:
+          'Nama aslinya adalah Ja’far Shadiq atau dikenal sebagai Raden Undung. Lahir pada 9 September 1400 Masehi. Sebutan Sunan Kudus tercipta karena beliau memilih Kudus sebagai tempat berdakwah terlamanya hingga bertahun-tahun. Ia merupakan putra Raden Usman Haji yang menyebarkan agama Islam di daerah Jipang Panolan, Blora, Jawa Tengah.'
+
+'Ia juga dikenal dengan julukan wali al-ilmi, karena sangat menguasai ilmu-ilmu agama, terutama tafsir, fikih, usul fikih, tauhid, hadits, serta logika. Sunan Kudus memiliki toleransi antar agama yang sangat tinggi. Dengan begitu, cara berdakwahnya adalah dengan mendekatkan agama Hindu Budha ke Islam.'
+
+'Ia mendapat kepercayaan untuk mengendalikan pemerintahan di daerah Kudus, sehingga ia menjadi pemimpin pemerintahan (Bupati) sekaligus pemimpin agama. Sunan Kudus meninggal di Kudus pada tahun 1550, makamnya berada di dalam kompleks Masjid Menara Kudus.',
+      image: 'assets/sunan-kudus.jpg'),
+  resep(
+      name: 'Raden ’Ainul Yaqin (Sunan Giri)',
+      htm: 'bukit giri gresik',
+      tutorial:
+          'Sunan Giri yang bernama asli Raden ’Ainul Yaqin merupakan putra dari Syekh Maulana Ishaq (murid Sunan Ampel), yang dikenal juga dengan Raden Paku. Beliau ditugsakan oleh Sunan Ampel untuk menyiarkan agama Islam di Blambangan. Sunan Giri pernah belajar di pesantren Ampel Denta lalu setelah dewasa, melalukan perjalanan haji bersama Sunan Bonang.'
+
+'Ketika hendak melaksanakan ibadah haji bersama Sunan Bonang, keduanya menyempatkan singgah di Pasai untuk memperdalam ilmu keimanan dan tasawuf. Pada sebuah kisah diceritakan bahwa Raden Paku sebagai salah satu nama-nama Walisongo bisa mencapai tingkatan ilmu laduni. Dengan prestasi tersebut Raden Paku dikenal juga dengan panggilan Raden ‘Ainul Yaqin. Sunan Giri meninggal sekitar awal abad ke-16, makam beliau ada di Bukit Giri, Gresik.',
+      image: 'assets/sunan-giri.jpg'),
+resep(
+      name: 'Raden Sahid (Sunan Kalijaga)',
+      htm: 'Demak',
+      tutorial:
+          'Sunan Kalijaga mempunyai nama kecil Raden Sahid beliau juga dijuluki Syekh Malaya, Ia adalah anak dari adipati Tuban bernama Tumenggung Wilatikta atau Raden Sahur. Agama Islam ia pelajari dari Sunan Bonang. Dari Sunan Bonanglah ia belajar menggunakan kesenian dan kebudayaan sebagai alat untuk menyebarkan agama Islam.'
+
+'Kesenian yang kerap ia gunakan untuk berdakwah adalah wayang kulit dan tembang suluk. Banyak masyarakat yang memercayai bahwa tembak suluk Lir-Ilir dan Gundul-Gundul Pacul adalah hasil karya Sunan Kalijaga.'
+
+'Sunan Kalijaga merupakan salah satu nama-nama Walisongo yang asli orang Jawa. Sunan Kalijaga meninggal pada pertengahan abad XV dan makamnya ada di desa Kadilangu, Kabupaten Demak, Jawa Tengah.',
+      image: 'assets/sunan-kalijaga.jpg'),resep(
+      name: 'Raden Umar Said (Sunan Muria)',
+      htm: 'bukit muria kudus',
+      tutorial:
+          'Raden Umar Said atau Sunan Muria adalah anak dari Sunan Kalijaga dengan Dewi Saroh. Namanya, Muria, diperkirakan oleh masyarakat sekitar Kota Kudus berasal dari nama gunung, yakni Gunung Muria. Gunung Muria itulah tempat di mana kini Sunan Muria dimakamkan. Ia dikenal sebagai Sunan Muria karena pusat dakwah dan bermukim beliau di Bukit Muria. Dalam sejarah tidak diketahui secara persis tahun meninggalnya dan menurut perkiraan, Sunan Muria meninggal pada abad ke-16 dan dimakamkan di Bukit Muria, Kudus.'
+
+'Gaya dakwah Sunan Muria pada umumnya mengambil metode yang digunakan ayahnya, Sunan Kalijaga, yakni menggunakan kesenian. Namun, Sunan Muria lebih senang tinggal jauh dari hiruk pikuk kota dan tinggal di daerah terpencil untuk menyebarkan agama. Ia juga turut mengajarkan cara bercocok tanam, jual beli dan melaut kepada rakyat jelata.',
+      image: 'assets/sunan-muria.jpg'),resep(
+      name: ' Syarif Hidayatullah (Sunan Gunung Jati)',
+      htm: 'cirebon banten',
+      tutorial:
+          'Sunan Gunung Jati yang memiliki nama asli Syarif Hidayatullah. Beliau banyak memberikan kontribusi dalam menyebarkan agama Islam di daerah Jawa Barat. Syarif Hidayatullah dikenal sebagai pendiri Kesultanan Cirebon dan Banten.'
+
+'Sunan Gunung Jati berasal dari Pasai.  Sunan Gunung Jati adalah pendiri dinasti kesultanan Banten yang dimulai dengan putranya, Sultan Maulana Hasanudin. Pada tahun 1527, Sunan Gunung Jati menyerang Sunda Kelapa di bawah pimpinan panglima perang Kesultanan Demak, Fatahillah.'
+
+'Ia merupakan sosok yang cerdas dan tekun dalam menuntut ilmu. Karena kesungguhannya, ia diizinkan ibunya untuk menuntut ilmu ke Makkah. Di sana, dia berguru pada  Syekh Tajudin Al-Qurthubi. Tak lama kemudian, ia lanjut ke Mesir dan berguru pada Syekh Muhammad Athaillah Al-Syadzili, ulama bermadzhab Syafi’i. Di sana, Sunan Gunung Jati belajar tasawuf tarekat syadziliyah.'
+
+'Sunan Gunung Jati wafat di Cirebon pada tahun 1570 dan usianya diperkiran sekitar 80 tahun. Makamnya terdapat di kompleks pemakaman Wukir Sapta Pangga di Gunung Jati, Desa Astana Cirebon, Jawa Barat.',
+      image: 'assets/sunan-gunung-jati.jpg'),
+];
